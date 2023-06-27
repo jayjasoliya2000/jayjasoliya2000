a8488b34e8883a4cd9b0123322551c28224c062b
Jay Jasoliya <jayjasoliya2000@gmail.com>
2023-06-27 16:18:54 +0530

    all done

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Navigation from './Compo/Navigation'
 import SearchEvent from './Compo/SearchEvent'
 import  Table  from './Compo/Table'
 import Crud from './Compo/Crud'
-import Resume from './Compo/Resume'
 
 export default function App() {
   return (
@@ -13,8 +12,7 @@ export default function App() {
 
       
     <Routes>
-       <Route path='/' element = {<Resume />}/>
-       <Route path='/search' element = {<SearchEvent />}/>
+       <Route path='/' element = {<SearchEvent />}/>
        <Route path='/table' element = {<Table />}/>
        <Route path='/crud' element = {<Crud />}/>
     </Routes>
